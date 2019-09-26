a30c40cbac3ba86cf9f3963a91b76ed4cafdd686
Matthew Houston <Matthew.Houston@camden.nsw.gov.au>
2019-09-27 08:19:14 +1000

    re-doing script

--- Sync-PrimaryAG.ps1
+++ Sync-PrimaryAG.ps1
@@ -1,10 +1,11 @@
-import-module .\dbatools
+import-module "$env:CamPSModulePath\dbatools\dbatools.psm1"
 
 If (Get-Cluster){
-    $clusterName = Get-Cluster
+    $clusterName = (Get-Cluster).Name
+    Write-Output "Cluster found : $clusterName "
 } else {
     Write-Error "Not member of cluster, exiting"
     exit
 }
 
-Sync-DbaAvailabilityGroup -Primary $clusterName | Out-Null
+#Sync-DbaAvailabilityGroup -Primary $clusterName -Secondary | Out-Null
