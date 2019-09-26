32aaf56b2b3afe290e2127e47f2519306376a9e1
Matthew Houston <Matthew.Houston@camden.nsw.gov.au>
2019-09-26 15:41:46 +1000

    ripped out most of the unessecary garbage

--- Sync-PrimaryAG.ps1
+++ Sync-PrimaryAG.ps1
@@ -1,24 +1,10 @@
-param(
-    [Parameter(Mandatory = $True)]
-    [string]
-    $clusterName
-)
+import-module .\dbatools
 
-import-module dbatools
-
-
-$availabiltiyGroups = Get-DbaAvailabilityGroup -SqlInstance $clusterName 
-
-Foreach ($availabiltiyGroup in $availabiltiyGroups){
-    $curPrimary = $availabiltiyGroup.PrimaryReplica
-    Write-Output "Current Primary is $curPrimary"
-    Foreach ($curReplica in $availabiltiyGroup.AvailabilityReplicas.Name){
-        If ($curReplica -ne $curPrimary){
-            Write-Output "Processing : Sync $curPrimary to replica $($curReplica)"
-            Sync-DbaAvailabilityGroup -Primary $curPrimary -Secondary $curReplica | Out-Null
-        }
-        else {
-            Write-Output "Replica Name ($curReplica) is the same as Primary Name ($curPrimary)"
-        }
-    }
+If (Get-Cluster){
+    $clusterName = Get-Cluster
+} else {
+    Write-Error "Not member of cluster, exiting"
+    exit
 }
+
+Sync-DbaAvailabilityGroup -Primary $clusterName | Out-Null
