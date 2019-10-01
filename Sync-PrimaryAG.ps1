310c3d79f14d3741f7f5217f1b30665b5c35a72d
Matthew Houston <Matthew.Houston@camden.nsw.gov.au>
2019-10-01 14:11:02 +1000

    Removed cluster check as no longer needed.

--- Sync-PrimaryAG.ps1
+++ Sync-PrimaryAG.ps1
@@ -1,13 +1,5 @@
 import-module dbatools
 
-If (Get-Cluster){
-    $clusterName = (Get-Cluster).Name
-    Write-Output "Cluster found : $clusterName "
-} else {
-    Write-Error "Not member of cluster, exiting"
-    exit
-}
-
 foreach ($curAvailabilityGroup in (Get-DbaAvailabilityGroup -SqlInstance $clusterName | Where-Object {$_.PrimaryReplica -eq $ENV:COMPUTERNAME})){
     Sync-DbaAvailabilityGroup -Primary $ENV:COMPUTERNAME -AvailabilityGroup $curAvailabilityGroup.AvailabilityGroup -WhatIf
 }
